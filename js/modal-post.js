50d8be10ec2d7e99c75ff6274e29d49d49807075
mQRain <roman.musalimov@gmail.com>
2018-04-06 23:44:40 +0300

    Fix v45
    Fix v45

--- js/modal-post.js
+++ js/modal-post.js
@@ -8,31 +8,31 @@ var email = popup.querySelector("[name=email]");
 var message = popup.querySelector("[name=message]");
 
 link.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.add("modal-feedback-show");
-    username.focus();
+	event.preventDefault();
+	popup.classList.add("modal-feedback-show");
+	username.focus();
 });
 
 close.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.remove("modal-feedback-show");
-    popup.classList.remove("modal-feedback-error");
+	event.preventDefault();
+	popup.classList.remove("modal-feedback-show");
+	popup.classList.remove("modal-feedback-error");
 });
 
 form.addEventListener("submit", function (event) {
-    if (!username.value || !email.value || !message.value) {
-        event.preventDefault();
-        popup.classList.remove("modal-feedback-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-feedback-error");
-    }
+	if (!username.value || !email.value || !message.value) {
+		event.preventDefault();
+		popup.classList.remove("modal-feedback-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-feedback-error");
+	}
 });
 
 window.addEventListener("keydown", function (event) {
-    if (event.keyCode === 27) {
-        if (popup.classList.contains("modal-feedback-show")) {
-            popup.classList.remove("modal-feedback-show");
-            popup.classList.remove("modal-feedback-error");
-        }
-    }
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("modal-feedback-show")) {
+			popup.classList.remove("modal-feedback-show");
+			popup.classList.remove("modal-feedback-error");
+		}
+	}
 });
